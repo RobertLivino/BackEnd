23f15241142c3d00e142250f133737a7d7e163c7
Robert Livino <robertlivino08@gmail.com>
2025-07-03 19:17:42 -0300

    commit

---
+++ gymApi/Data/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using gymApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace gymApi.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Exercise> Exercises { get; set; }
+    }
+}
---
+++ gymApi/Models/Exercise.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gymApi.Models
+{
+    public class Exercise
+    {
+        public int Id { get; set; }
+        public string ExerciseName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string Equipment { get; set; } = string.Empty;
+        public string TargetMuscle { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
